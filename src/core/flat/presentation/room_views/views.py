4aff82ae42ab1d2eec38973dd2ba3be7368f0815
xcixor <pndungu54@gmail.com>
2020-01-12 04:30:31 +0300

    chore(refactor): convert function based request to class based requests

--- src/core/flat/presentation/room_views/views.py
+++ src/core/flat/presentation/room_views/views.py
@@ -1,49 +1,19 @@
 """Contains room related views."""
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from rest_framework import generics
 from flat.models import Room
 from flat.serializers import RoomSerializer
 
-@api_view(['GET', 'POST'])
-def room_list(request, format=None):
+class RoomList(generics.ListCreateAPIView):
     """
-    Get all rooms or create new
+    List all room, or create a new room.
     """
-    if request.method == 'GET':
-        rooms = Room.objects.all()
-        serializer = RoomSerializer(rooms, many=True)
-        return Response(serializer.data)
-
-    elif request.method == 'POST':
-        serializer = RoomSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
 
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def room_detail(request, pk, format=None):
+class RoomDetail(generics.RetrieveUpdateDestroyAPIView):
     """
-    Retrieve, update or delete a room.
+    Retrieve, update or delete a room instance.
     """
-    try:
-        room = Room.objects.get(pk=pk)
-    except Room.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        serializer = RoomSerializer(room)
-        return Response(serializer.data)
-
-    elif request.method == 'PUT':
-        serializer = RoomSerializer(room, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    elif request.method == 'DELETE':
-        room.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
--- src/core/flat/presentation/urls.py
+++ src/core/flat/presentation/urls.py
@@ -4,8 +4,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import room_views
 
 urlpatterns = [
-    path('rooms/', room_views.room_list),
-    path('rooms/<int:pk>/', room_views.room_detail),
+    path('rooms/', room_views.RoomList.as_view()),
+    path('rooms/<int:pk>/', room_views.RoomDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
